f3fe743f26b249c3e9de6dc57a04c4cd54596241
Manan <mananvyas@gmail.com>
2020-03-08 17:47:42 +0530

    Enabled HTTPS SSL support

--- basicauth/src/main/java/com/practice/springbootsecurity/basicauth/BasicauthApplication.java
+++ basicauth/src/main/java/com/practice/springbootsecurity/basicauth/BasicauthApplication.java
@@ -1,8 +1,15 @@
 package com.practice.springbootsecurity.basicauth;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 //@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
@@ -14,4 +21,37 @@ public class BasicauthApplication {
 		SpringApplication.run(BasicauthApplication.class, args);
 	}
 
+	@Bean
+	public ServletWebServerFactory servletContainer(){
+		TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+
+				SecurityCollection securityCollection = new SecurityCollection();
+				securityCollection.addPattern("/*");
+				securityConstraint.addCollection(securityCollection);
+				context.addConstraint(securityConstraint);
+			}
+
+		};
+
+		tomcatServletWebServerFactory.addAdditionalTomcatConnectors();
+		return tomcatServletWebServerFactory;
+
+	}
+
+	private Connector httpToHttpRedirectConnector(){
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
 }
+
+
+
